1aed170b987da03df2023e376b8f47f05b3be6e0
Jonathan Rochkind <jonathan@dnil.net>
2015-06-01 17:19:41 -0400

    precompile stackview images please, rails

--- lib/rails_stackview/engine.rb
+++ lib/rails_stackview/engine.rb
@@ -1,4 +1,7 @@
 module RailsStackview
   class Engine < ::Rails::Engine
+    # We need to precompile our stackview pngs, not sure what Rails
+    # is up to. 
+    config.assets.precompile += %w[stackview/*.png stackview/*.gif stackview/*.jpg] 
   end
 end

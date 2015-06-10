97f2f2a61368c9f52f247f2deb2fab4d425d28cd
Jonathan Rochkind <jonathan@dnil.net>
2015-06-10 11:54:47 -0400

    add missing assertions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,3 +17,26 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
   ActiveSupport::TestCase.fixtures :all
 end
+
+# Add some useful assertions that minitest annoyingly leaves out
+module Minitest::Assertions
+  def assert_length(length, obj, msg = nil)
+    unless obj.respond_to? :length
+      raise ArgumentError, "object with assert_length must respond_to? :length", obj
+    end
+
+    msg ||= "Expected length of #{obj} to be #{length}, but was #{obj.length}"
+
+    assert_equal(length, obj.length, msg.to_s )
+  end
+
+  def assert_present(obj)
+    unless obj.respond_to? :present?
+      raise ArgumentError, "object with assert_present must respond_to? :present?", obj
+    end
+
+    msg ||= "Expected #{obj}.present?"
+
+    assert obj.present?, msg
+  end
+end

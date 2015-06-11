13eb2a1cf951dab41a22e6f32138f2d65227d0d3
Jonathan Rochkind <jonathan@dnil.net>
2015-06-11 17:35:38 -0400

    adjust order of JS load to make sure stackview init happens right

--- app/assets/javascripts/rails_stackview.js
+++ app/assets/javascripts/rails_stackview.js
@@ -1,6 +1,11 @@
 // rails_stackview sprockets JS manifest that includes vendored stackview JS as well as custom JS
 //
 //= require jquery.stackview.js
-//= require ./rails_stackview/auto_init.js
 //= require ./rails_stackview/plain.js
+//
+// Important to load browser, which adjusts the size of the stackview div,
+// BEFORE auto-init, to make sure the div is the right size when stackview
+// gets init'd, since it will base some things on the size of the div. 
 //= require ./rails_stackview/browser.js
+//
+//= require ./rails_stackview/auto_init.js

609f98d33029e25aeeea070c5e742c3c6789359e
Jonathan Rochkind <jonathan@dnil.net>
2015-06-15 10:19:08 -0400

    browser, add ajax item load

--- app/assets/javascripts/rails_stackview/browser.js
+++ app/assets/javascripts/rails_stackview/browser.js
@@ -6,11 +6,43 @@
     $(".shelfbrowser").css("height", $(window).height() - topOffset);
   }
 
+  function loadItem(base_url, panel, item) {
+    $.ajax({
+      url: base_url,
+      data: {
+        'id': item.system_id,
+        'sort_key': item.sort_key,
+        'sort_key_display': item.sort_key_display,
+        'sort_key_type': item.sort_key_type
+      },
+      dataType: "html",
+      success: function(html, status, xhr) {
+        panel.html( html );
+      }
+    });
+  }
+
   $( document ).on("ready", function() {
     fitToWindowHeight();
   });
   $( window ).on("resize", function() {
     fitToWindowHeight();
+  });
+
+  // If stackview_browser_item_path is defined, click on item
+  // should load partial via AJAX, and preventDefault. 
+  $(document).on("click", ".shelfbrowser .stack-item a", function(event) {
+    var target        = $(event.target);
+    var item_load_url = target.closest(".shelfbrowser-browse-column").data('stackviewBrowserItemPath');
+    var panel         = target.closest(".shelfbrowser").find(".shelfbrowser-info-column .stack-item-panel").filter(":visible");
+
+    if(item_load_url && panel.length > 0) {
+      event.preventDefault();   
+
+      var item_attribute_hash = target.closest(".stack-item").data("stackviewItem");
+
+      loadItem(item_load_url,  panel,  item_attribute_hash );
+    }
   });
 
 })(jQuery);
--- test/dummy/app/controllers/demo_controller.rb
+++ test/dummy/app/controllers/demo_controller.rb
@@ -5,4 +5,10 @@ class DemoController < ApplicationController
   def browse
     render :template => 'rails_stackview/browser', :locals => {:origin_sort_key => params["origin_sort_key"] || "M"}
   end
+
+  # Return partial HTML used for browser to display item on page
+  # For this demo, we just echo back the params. 
+  def browse_partial
+    render :layout => false, :html => params.to_json
+  end
 end
